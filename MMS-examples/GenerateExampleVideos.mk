ba19996f290da68a4749b4533f029c177d1c51fa
Fabrizio Nunnari <fab.nunnari@gmail.com>
2026-07-30 10:48:20 +0200

    Update GenerateExampleVideos.mk

--- MMS-examples/GenerateExampleVideos.mk
+++ MMS-examples/GenerateExampleVideos.mk
@@ -8,8 +8,8 @@ ifndef BLENDER_EXE
 $(error BLENDER_EXE variable is not set)
 endif
 
-ifndef AVASAG_CORPUS_DIR
-$(error AVASAG_CORPUS_DIR variable is not set)
+ifndef DICTIONARY_DIR
+$(error DICTIONARY_DIR variable is not set)
 endif
 
 # Gets the list of MMS files and replace the suffix to mp4 to generate the list of targets
@@ -21,8 +21,10 @@ all: $(OUT_VIDEO_FILES)
 
 # Main rule to create a video from an MMS
 %.mp4: %.mms.csv
-	$(BLENDER_EXE) --background --python main.py -- --corpus-generated-directory "$(AVASAG_CORPUS_DIR)/generated/" --source-mms-file "$<" --use-relative-time --export-mp4 "$@" --render-size-pct 50 --log-to-console
+	$(BLENDER_EXE) --background --python main.py -- --corpus-generated-directory "$(DICTIONARY_DIR)" --source-mms-file "$<" --use-relative-time --export-mp4 "$@" --render-size-pct 50 --log-to-console
 
+clean:
+	rm $(OUT_VIDEO_FILES)
 
 touch:
 	@touch Examples-MMS/*.mms.csv
